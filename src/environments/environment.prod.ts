f4b434d1f29d229f57a13bbed7fdf8a6aa33a754
Arif-Iquba'l <arif04102001@gmail.com>
2026-05-15 11:49:59 +0530

    [Arif Iqubal] : fix environment url for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'hireconnect-backend-prod-production.up.railway.app'
+  apiUrl: 'https://hireconnect-backend-prod-production.up.railway.app'
 };
