ddf790f35684e6144818435189b82c31f96f7472
Arif-Iquba'l <arif04102001@gmail.com>
2026-05-15 11:58:43 +0530

    [Arif Iqubal] : centralize api v1 base path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hireconnect-backend-prod-production.up.railway.app'
+  apiUrl: 'https://hireconnect-backend-prod-production.up.railway.app/api/v1'
 };
